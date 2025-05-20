e700aa011fd7b169759b12806477bb39112cb64a
midaward <midawardisaac@gmail.com>
2025-05-20 00:36:13 -0500

    Adding url next dates

---
+++ src/puzzles/puzzles.ts
@@ -0,0 +1,18 @@
+export const PUZZLES=[
+    {
+        id:"upper-right",
+        name:"Dinero"
+    },
+    {
+        id:"upper-left",
+        name:"Confianza"
+    },
+    {
+        id:"bottom-left",
+        name:"Trabajo"
+    },
+    {
+        id:"bottom-right",
+        name:"Relaciones"
+    }
+]
